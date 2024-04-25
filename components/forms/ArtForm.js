3a660dd55018febacd52f086d22ba71efd44e691
zach colburn <colburn.zach7@gmail.com>
2024-04-24 19:25:04 -0500

    Art form has filler data in it but need data from backend still

--- components/forms/ArtForm.js
+++ components/forms/ArtForm.js
@@ -2,23 +2,38 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-function BasicExample() {
+function ArtForm() {
   return (
     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We will never share your email with anyone else.
-        </Form.Text>
+      <Form.Group className="mb-3" controlId="formBasicImage">
+        <Form.Label>Image</Form.Label>
+        <Form.Control type="Image" placeholder="Image" />
+        <Form.Text className="text-muted" />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Label>Title</Form.Label>
+        <Form.Control type="password" placeholder="Art Title" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Details</Form.Label>
+        <Form.Control type="password" placeholder="Art Description" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Categories</Form.Label>
+        <Form.Select aria-label="Default select example">
+          <option>Select</option>
+          <option value="1">Surrealism</option>
+          <option value="2">Impressionism</option>
+          <option value="3">Abstract Expressionism</option>
+          <option value="4">Realism</option>
+          <option value="5">Cubism</option>
+          <option value="6">Portraiture</option>
+          <option value="7">Still Life</option>
+          <option value="8">Romanticism</option>
+        </Form.Select>
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
@@ -27,4 +42,4 @@ function BasicExample() {
   );
 }
 
-export default BasicExample;
+export default ArtForm;
