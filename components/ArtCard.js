566f946207940b3f9ca01ccf3662f4f6783b9327
Ross Morgan <ross.morgan933@gmail.com>
2024-04-27 09:47:45 -0500

    added api calls and card renders to page

---
+++ components/ArtCard.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteArt } from '../api/artData';
+
+function ArtCard({ artObj, onUpdate }) {
+  // FOR DELETE, WE NEED TO REMOVE THE BOOK AND HAVE THE VIEW RERENDER,
+  // SO WE PASS THE FUNCTION FROM THE PARENT THAT GETS THE BOOKS
+  const deleteThisArt = () => {
+    if (window.confirm(`Delete ${artObj.title}?`)) {
+      deleteArt(artObj.id).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={artObj.imageUrl} alt={artObj.title} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>{artObj.title}</Card.Title>
+        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
+        <Link href={`/art/edit${artObj.id}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisArt} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+ArtCard.propTypes = {
+  artObj: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    imageUrl: PropTypes.string,
+    userId: PropTypes.string,
+    tags: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default ArtCard;
--- utils/auth.js
+++ utils/auth.js
@@ -2,12 +2,11 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import { clientCredentials } from './client';
 
+const dbUrl = clientCredentials.databaseURL;
+
 const checkUser = (uid) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/checkuser`, {
-    method: 'POST',
-    body: JSON.stringify({
-      uid,
-    }),
+  fetch(`${dbUrl}/checkuser/${uid}`, {
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
@@ -17,10 +16,10 @@ const checkUser = (uid) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-const registerUser = (userInfo) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/register`, {
+const registerUser = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/register`, {
     method: 'POST',
-    body: JSON.stringify(userInfo),
+    body: JSON.stringify(payload),
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
